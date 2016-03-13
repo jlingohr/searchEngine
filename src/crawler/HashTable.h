107ea7c8a6819cbb283cba796da1b36d7e184297
Johann Lingohr <johannlingohr@gmail.com>
2016-03-13 00:06:09 -0800

    add to hash

--- src/crawler/HashTable.h
+++ src/crawler/HashTable.h
@@ -13,8 +13,17 @@ post: values of table are set to NULL
 void initDictionary(DICTIONARY* table);
 
 /*
-  addNodeToDictionary: Adds node for seed to dict if seed is unique
-  
+  addNodeToDictionary: Adds node for seed to dict if seed is unique. If dict
+  is empty, create DNODE at hashed value and set start = end = DNODE. Otherwise,
+  check if dict[hash] is empty. If it is, insert DNODE at dict[hash] and set pointers.
+  Otherwise traverse linked-list to check uniqueness.
+
 **/
-void addNodeToDictionary(DICTIONARY* dict, char* seed, URLNODE* node);
+void addNodeToDictionary(DICTIONARY* dict, char* seed, void* node);
+
+/*
+  initDNODE: Initializes a DNODE by copying in values from urlNode
+  Set next/prev pointers to NULL, data to urlNode, and copy in key
+**/
+void initDNODE(DNODE* dnode, char* key, void* urlNode);
 #endif
