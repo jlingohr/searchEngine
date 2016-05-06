f796f69244c67639c602de41eb2e010c197dd20a
Johann Lingohr <johannlingohr@gmail.com>
2016-05-05 19:46:22 -0700

    Start to build index

--- indexer/src/indexer.c
+++ indexer/src/indexer.c
@@ -4,6 +4,8 @@
 * 
 * For testing ability to reconstruct the inverted index
 * ./indexer  [TARGET_DIRECTORY] [RESULTS FILENAME] [RESULTS FILENAME] [REWRITEN FILENAME]
+*
+* To run: ./index ../crawler/data/ ./data/index.dat
 */
 
 #include <stdio.h>                           // printf
@@ -33,6 +35,9 @@ HashTable* Index;
 
 int main(int argc, char** argv) {
   char *target_directory, *target_file, *test_old, *test_new;
+  char* doc, *prev_file;
+  char** filenames;
+  int num_files;
   
 
   /*1. Program parameter processing */
@@ -45,18 +50,36 @@ int main(int argc, char** argv) {
        allocate Inverted_index, zero it, and set links to NULL. */
   Index = initHashTable();
 
-  //3. LOG( "Building the index" );
-  if (STATUS_LOG == 1) {
-    printf("\nBuilding the index\n");
-  }
-
-  //4. Index *wordindex=buildIndexFromDirectory("argv[1]");
   target_directory = argv[1];
   target_file = argv[2];
   if (argc == 5) {
     test_old = argv[3];
     test_new = argv[4];
   }
+  num_files = getFileNamesInDir(target_directory, &filenames);
+  if (num_files < 0) {
+    fprintf(stderr, "Failure reading filenames.\n");
+    /* TODO - Free memory */
+    return 1;
+  }
+
+  /* Prepend directory name to filenames?
+  for (int i = 0; i < num_files; i++) {
+    printf("%s\n", filenames[i]);
+    
+  } */
+  
+
+  //3. LOG( "Building the index" );
+  if (STATUS_LOG == 1) {
+    printf("\nBuilding the index\n");
+  }
+
+  /*4. Index *wordindex=buildIndexFromDirectory("argv[1]"); */
+  for (int i = 0; i < num_files; i++) {
+    doc = loadDoc(filenames[i]);
+  }
+
 
   //5. saveFile (argv[2], wordindex );
     // LOG( "done!");
@@ -92,7 +115,7 @@ int checkCommandLine(int argc, char** argv) {
   char* dir, *file;
   int index;
   FILE* fp;
-  if (argc != 3 || argc != 5)
+  if (argc != 3 && argc != 5)
     return 0;
 
   dir = argv[1];
@@ -105,17 +128,15 @@ int checkCommandLine(int argc, char** argv) {
     strcpy(dir, argv[1]);
     strcat(dir, "/");
   }
-  if (!isDirec(dir))
+  //printf("Checking directory: %s\n", dir);
+  if (!isDir(dir))
     return 0;
 
   /* Validate file */
-  fp = Fopen(file, "r");
-  if ((fp = Fopen(file, "r"))) {
-    fprintf(stderr, "File already exists\n");
-    Fclose(fp);
+  if (isFile(file)) {
+    fprintf(stderr, "File %s already exists\n", file);
     return 0;
   }
-  Fclose(fp);
 
   if (argc == 5) { /* For testing */
     char* old;
@@ -129,15 +150,74 @@ int checkCommandLine(int argc, char** argv) {
       return 0;
     }
 
-    if ((fp = Fopen(new, "r"))) {
-      fprintf(stderr, "File already exists\n");
-      Fclose(fp);
+    if (isFile(new)) {
+      fprintf(stderr, "File %s already exists\n", new);
       return 0;
     }
-    Fclose(fp);
 
   }
 
   return 1;
 
+}
+
+/*
+* loadDoc - Loads the HTML document from a file
+* @filename: name of file to be loaded
+*
+* Returns string containing the loaded document
+*/
+char* loadDoc(char* filename) {
+  //TODO
+  return NULL;
+}
+
+/*
+* getDocID - Generates document identifier from the name of the file. ASSUME
+* crawler saved the filed using unique identifiers as names
+* @doc: name of the file
+*
+* Returns integer identifying document
+*/
+int getDocID(char* filename) {
+  //TODO
+  return -1;
+}
+
+/*
+* getNextWord - Parses the string containing the loaded document skippng the HTML tags
+* @doc: string containing the HTML document
+* @pos: current position within that document (starts at 0)
+* @word: pointer to C-style string
+*
+* Returns the current position in the document and a newly allocated word (must be freed)
+*/
+int getNextWord(const char* doc, int pos, char** word) {
+  // TODO
+  return -1;
+}
+
+/* updateIndex - Updates the structure containing the index
+* @word: string containing the word
+* @docID: identifier of the document
+* @index: InvertedIndex hashtable
+*
+* Returns 1 if successful
+* Returns 0 otherwise
+*/
+int updateIndex(char* word, int docID, InvertedIndex* index) {
+  //TODO
+  return 0;
+}
+
+/*
+* saveIndexToFile - Saves the inverted index to a file in .dat format
+* @index: Pointer to the inverted index
+*
+* Returns 1 if successful
+* Returns 0 otherwise
+*/
+int saveIndexToFile(InvertedIndex* index) {
+  //TODO
+  return 0;
 }
