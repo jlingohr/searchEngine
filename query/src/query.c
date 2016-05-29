6d10cb1358eb53efaf659fcad5b742db340109b0
Johann Lingohr <johannlingohr@gmail.com>
2016-05-28 23:22:44 -0700

    Updated query; need to work on mergesort and merge

--- query/src/query.c
+++ query/src/query.c
@@ -19,13 +19,15 @@ static char prompt[] = "Query:> ";
 int checkCommandLine(char* filename, char* path);
 void eval(char* cmdline);
 void ToLower(char* word);
+int str_compare(element_t av, element_t bv);
+void free_string(element_t av);
 
 int main(int argc, char** argv) {
   /* TODO */
   char cmdline[MAXLINE];
   char* filename, *html_path;
   int emit_prompt;
-  HashTable* index;
+  HashTable* Index;
   Query* query;
 
   emit_prompt = 1;
@@ -54,7 +56,10 @@ int main(int argc, char** argv) {
     filename = argv[1];
     html_path = argv[2];
     /* Build of index from recent file*/
-    index = readFile(filename);
+    //index = readFile(filename);
+    Index = malloc(sizeof(HashTable));
+    hashtable_new(Index, sizeof(WordNode), wNode_cmp, wNode_hash, wNode_free);
+    readFile(Index, filename);
 
     
 
@@ -64,7 +69,7 @@ int main(int argc, char** argv) {
     query = initQuery(cmdline);
     printf("Query initialized...\n");
     /* Handle the query */
-    HandleQuery(index, query);
+    HandleQuery(Index, query);
 
     /* Parse user query */
     //num_words = HandleQuery(cmdline, &query);
@@ -114,10 +119,17 @@ int checkCommandLine(char* filename, char* path) {
 * Returns a pointer to a Query struct
 */
  Query* initQuery(char* str) {
-  /* Allocate Query struct */
+  // Allocate Query struct
   Query* query = malloc(sizeof(Query));
-  query->terms = initList();
-  query->ops = initList();
+  //query->terms = initList();
+  //query->ops = initList();
+  query->terms = malloc(sizeof(List));
+  query->ops = malloc(sizeof(List));
+  
+  // Initialize the lists
+  list_new(query->terms, sizeof(char*), str_compare, free_string);
+  list_new(query->ops, sizeof(char*), str_compare, free_string);
+
 
   /* Normalize search string */
   query->num_sets = parseQuery(str, query->terms, query->ops);
@@ -151,17 +163,21 @@ int parseQuery(char* str, List* terms, List* ops) {
 
     if (strcmp(word, "AND") == 0) { 
       /* Append to ops list */
-      listAdd(ops, word);
+      //listAdd(ops, word);
+      list_append(ops, word);
     }
     else if (strcmp(word, "OR") == 0) {
-      listAdd(ops, word);
+      //listAdd(ops, word);
+      list_append(ops, word);
       sets++;
     }
     else {
       /* Not logical operator! */
       ToLower(word);
-      listAdd(terms, word);
+      //listAdd(terms, word);
+      list_append(terms, word);
     }
+    free(word);
 
     tokens++;
     pch = strtok(NULL, " ");
@@ -210,9 +226,8 @@ void ToLower(char* word) {
 */
 void HandleQuery(HashTable* ht, Query* query) {
   /* TODO - Improve this! Too many casts */
-  //List *tmp_c; /* List of pointers to lists containing documents */
-  List* docs;                           /* Primary list of documents */
-  ListNode *opNode;
+
+  List* docs;
   int filled, num_sets;
 
   filled = 0;
@@ -223,32 +238,32 @@ void HandleQuery(HashTable* ht, Query* query) {
   sets[filled] = temp_a;
   filled++;
 
-  while (filled < num_sets) {
-    opNode = listRemove(query->ops);
-    char* op = (char*)opNode->data;
+  // Store operand, i.e. AND, OR
+  char op[MAXLINE];
+  while(filled < num_sets) {
+    list_dequeue(query->ops, &op);
 
     if (strcmp(op, "AND") == 0) {
       temp_a = getNextQuery(ht, query->terms);
       sets[filled] = intersect(sets[filled], temp_a);
     }
-    else if(strcmp(op, "OR") == 0) {
+    else if (strcmp(op, "OR") == 0) {
       filled++;
       sets[filled] = getNextQuery(ht, query->terms);
     }
   }
   printf("Merging lists...\n");
 
-  /* Sort the list and return */
-  for (int i = 0; i < query->num_sets; i++) {
+  //Sort the lists
+  /*for (int i = 0; i < query->num_sets; i++) {
     MergeSort(sets[i], sets[i]->len, cmpDNode_freq);
   }
-  /* Merge all documents */
+  // Merge all documents 
   for (int i = 0; i < query->num_sets; i++) {
     docs = Merge(docs, sets[i], cmpDNode_freq);
-  }
+  }*/
 
-  listForEach(printDNode, docs);
-
+  
 
 }
 
@@ -259,13 +274,23 @@ void HandleQuery(HashTable* ht, Query* query) {
 * @words: list of words nodes to get query from
 */
 List* getNextQuery(HashTable* ht, List* words) {
-  /* TODO - Make sure this is clean */
+  // TODO - Account for refactoring
+  /*
   ListNode* tmpNode = listRemove(words);
   char* word = (char*)tmpNode->data;
 
   WordNode* wNode = IndexGet(ht, word);
   List* acc = wNode->page;
   return acc;
+  */
+  // Dequeue word from words
+  char term[MAXLINE];
+  list_dequeue(words, &term);
+
+  WordNode* wNode = malloc(sizeof(WordNode));
+  hashtable_get(ht, term, wNode);
+  return wNode->page;
+
 }
 
 /*
@@ -277,48 +302,48 @@ ListNode* getNextOp(List* list) {
 }
 
 /*
-* intersect - intersect two lists
+* intersect - intersect two lists of DocumentNodes
 * Returns pointer to a new list
 */
 List* intersect(List* A, List* B) {
-  /* TODO - NOTE: HUGE mem leaks! */
-  List* acc;
-  ListNode *cur, *tmp;
+  // TODO - NOTE: HUGE mem leaks!
+  List* list;
+  //ListNode *cur, *tmp;
 
 
-  if (A == NULL || B == NULL) /* empty sets */
+  if (A == NULL || B == NULL) // empty sets 
     return NULL;
 
-  /* Initialize new list */
-  acc = initList();
+  // Initialize new list 
+  list = malloc(sizeof(List));
+  list_new(list, sizeof(DocumentNode), cmpDNode_ID, NULL);
 
-  /* Set current pointer to smaller list */
-  if (A->len <= B->len) { /* Have to check if NULL returned */
-    while((cur = listRemove(A))) {
-      DocumentNode* dNode = (DocumentNode*)cur->data;
-      if ((tmp = listGet(B, (element_t)dNode, cmpDNode_ID))) { /* shared item */
-        //DocumentNode* dNode = (DocumentNode*)cur->data;
-        /* Update document node */
-        update(dNode, (DocumentNode*)tmp->data);
-        /* Add updated node to accumulated list */
-        listAdd(acc, dNode);
+  // Set current pointer to smaller list 
+  DocumentNode cur;
+  DocumentNode tmp;
+  if (A->length <= B->length) { //Have to check if NULL returned
+    while (A->length) {
+      list_dequeue(A, &cur);
+      if (list_get(B, (element_t)&cur.document_id, (element_t)&tmp)) {
+        // Update Document Node
+        update(&cur, &tmp);
+        // Add updated node to accumulated list
+        list_append(list, &cur);
       }
     }
   }
   else {
-    while((cur = listRemove(A))) {
-      DocumentNode* dNode = (DocumentNode*)cur->data;
-      if ((tmp = listGet(B, (element_t)dNode, cmpDNode_ID))) { /* shared item */
-        //DocumentNode* dNode = (DocumentNode*)cur->data;
-        /* Update document node */
-        update(dNode, (DocumentNode*)tmp->data);
-        /* Add updated node to accumulated list */
-        listAdd(acc, dNode);
+    while (B->length) {
+      list_dequeue(B, &cur);
+      if (list_get(A, (element_t)&cur.document_id, (element_t)&tmp)) {
+        update(&cur, &tmp);
+        list_append(list, &cur);
       }
     }
-  } 
-  /* Returns list */
-  return acc;
+  }
+
+  // Returns list 
+  return list;
 }
 
 
@@ -368,4 +393,17 @@ void printDNode(const element_t av) {
   DocumentNode* a = (DocumentNode*)av;
   printf("%d, %d\n", a->document_id, a->page_word_frequency);
 
+}
+
+int str_compare(element_t av, element_t bv)
+{
+  char* a = av;
+  char* b = bv;
+  return strcmp(a, b) == 0;
+}
+
+void free_string(element_t av)
+{
+  char* a = av;
+  free(a);
 }
